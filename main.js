9cc179ad10dc397bceeaf2f5446103c997513013
akashsrkar <akashsarkar.dev@gmail.com>
2024-07-05 22:23:32 +0530

    add: colored tiles onload

--- main.js
+++ main.js
@@ -4,13 +4,20 @@ const colorPickList = [...colors, ...colors].sort((a, b) => Math.random() - 0.5)
 const tileCount = colorPickList.length;
 
 
-let revealedTileCount = null;
+let revealedTileCount = 0;
 let activeTile = null;
 let awaitForCheck = false;
 
 for(let i = 0; i < tileCount; i++){
     const elem = document.createElement("div");
     elem.classList.add("tile");
+    elem.setAttribute("data-color", colorPickList[i]);
+    elem.addEventListener("click", onTileClick)
 
     tilesContainer.appendChild(elem);
+}
+
+
+function onTileClick(e){
+
 }
